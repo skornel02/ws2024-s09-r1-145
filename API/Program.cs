ea6d8d183535871d530e07fa7d374e5f445c074d
Kornél <stefankornel02@google.com>
2023-02-22 21:58:34 +0100

    Added frontend project Configured backend to serve it Backend automatically extracts hosted files Basic sveltekit project

--- API/Program.cs
+++ API/Program.cs
@@ -9,8 +9,11 @@ namespace API;
 
 public class Program
 {
-    public static void Main(string[] args)
+
+    public static async Task Main(string[] args)
     {
+        await InstallFiles();
+
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddTransient<IAuthenticationService, HardcodedAuthenticationService>();
@@ -33,9 +36,43 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        app.UseStaticFiles();
+
         app.MapAuthentication();
-        app.MapRoutes();
+
+        app.MapFallbackToFile("index.html");
 
         app.Run();
+    }
+
+    private static async Task InstallFiles()
+    {
+        var basePath = AppDomain.CurrentDomain.BaseDirectory;
+        var wwwrootPath = Path.Combine(basePath, "wwwroot");
+
+        if (!Directory.Exists(wwwrootPath))
+        {
+            var assembly = typeof(Program).Assembly;
+            Directory.CreateDirectory(wwwrootPath);
+
+            var resourcePaths = assembly.GetManifestResourceNames()
+                .Where(rnn => rnn.Contains("wwwroot"))
+                .ToList();
+
+            foreach (var resourcePath in resourcePaths)
+            {
+                var fileName = resourcePath;
+                var filePath = Path.Combine(basePath, fileName);
+                var fileInfo = new FileInfo(filePath);
+                fileInfo.Directory?.Create();
+                using var stream = File.Create(filePath);
+                using var resourceStream = assembly.GetManifestResourceStream(resourcePath);
+
+                if (stream != null && resourceStream != null)
+                {
+                    await resourceStream.CopyToAsync(stream);
+                }
+            }
+        };
     }
 }
